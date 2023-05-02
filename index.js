2a4735b50b7be674f9a5ac72f0e81a3a9c36c6aa
Victor Gyozo Vancsura <gyozovancsura@gmail.com>
2023-05-02 12:25:15 +0100

    Added date using Javascript

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-var datetime = new Date();
-console.log(datetime);
-document.getElementById("date").textContent = datetime;
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
